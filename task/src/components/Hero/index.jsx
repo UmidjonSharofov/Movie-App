6868c78b8500b8213b0d8b6e2e93f7ab3ca3ca8c
umid <umidshrofov@gmail.com>
2023-03-25 21:20:58 +0500

    button

--- task/src/components/Hero/index.jsx
+++ task/src/components/Hero/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+import Button from '../Button'
 import { Contanier, Description, SubTitle, Title, Wrapper } from './main'
-
+import {CiPlay1} from 'react-icons/ci'
 export default function Hero() {
     return (
     <Contanier>
@@ -8,6 +9,10 @@ export default function Hero() {
           <Title>STAR WARS</Title>
           <SubTitle>THE RISE OF SKYWALKER</SubTitle>
           <Description>The surviving members of the resistance face the First Order once again, and the legendary conflict between the Jedi and the Sith reaches its peak bringing the Skywalker saga to its end. </Description>
+          <div>
+            <Button icon={<CiPlay1/>} >Watch Now</Button>
+            <Button>Trailer</Button>
+          </div>
       </Wrapper>
     </Contanier>
  )
